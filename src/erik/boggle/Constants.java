11d5044c29ac46aaf5fd9ab56569cd19b5635f2a
eai500 <eai500@gmail.com>
2014-01-04 11:07:14 -0600

    Delete Constants.java

--- src/erik/boggle/Constants.java
+++
@@ -1,9 +0,0 @@
-package erik.boggle;
-
-/**
- * @author erik
- */
-public class Constants {
-    public static final String DEFINITION_WS_BASE_URL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/";
-    public static final String DEFINITION_WS_KEY = "d54724b5-f0e9-471e-93ac-58e294454af7";
-}
